3e4f170442fb259383d436477aa45cdbc7966d78
Hong-en Hsu <hongaenkimo@yahoo.com.tw>
2019-09-18 02:46:16 +0800

    #新增 TYPEFINDER #新增 啟動器 #加入 Unity Ioc 容器

---
+++ Template/Template.Core/Finder/TypeFinder.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template.Core.Finder
+{
+    public class TypeFinder : ITypeFinder
+    {
+        public IEnumerable<Type> GetTypeByClasses<T>(bool onlyConcreteClasses = true)
+        {
+
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            var assignType = typeof(T);
+            var result = new List<Type>();
+
+            foreach (var assemblie in assemblies)
+            {
+                var types = assemblie.GetTypes();
+                if (types == null)
+                {
+                    continue;
+                }
+
+                var list = types.Where(o =>
+                    (assignType.IsAssignableFrom(o) || (assignType.IsGenericParameter && TypeImplementOpenGenerics(o, assignType)))
+                    && !o.IsInterface
+                );
+                if (onlyConcreteClasses)
+                {
+                    list = list.Where(o => o.IsClass && !o.IsAbstract);
+                }
+                result.AddRange(list);
+
+            }
+            return result;
+        }
+
+        private bool TypeImplementOpenGenerics(Type type, Type assignType)
+        {
+            try
+            {
+                var genericTypeDefinition = assignType.GetGenericTypeDefinition();
+                foreach (var item in type.FindInterfaces((objType, objCriteria) => true, null))
+                {
+                    if (!item.IsGenericType)
+                    {
+                        continue;
+                    }
+                    return genericTypeDefinition.IsAssignableFrom(item.GetGenericTypeDefinition());
+                }
+                return false;
+            }
+            catch
+            {
+
+                return false;
+            }
+
+
+
+        }
+    }
+}
---
+++ Template/Template.Core/Starter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template.Core.DependencyManagement;
+using Unity;
+
+namespace Template.Core
+{
+    public class Starter : IStarter
+    {
+        private IocManager _iocManager;
+        public IocManager IocManager { get => _iocManager; set => _iocManager = value; }
+        public void Initialize()
+        {
+            _iocManager = new IocManager(new UnityContainer());
+            _iocManager.Container.RegisterInstance<IStarter>(this);
+
+
+        }
+
+
+    }
+}
